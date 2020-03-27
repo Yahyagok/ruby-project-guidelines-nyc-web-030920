aff3130db219d327065ba798123fcbbc82fb9abd
Yahyagok <ygok17@hotmail.com>
2020-03-27 11:38:43 -0400

    update

--- bin/cli.rb
+++ bin/cli.rb
@@ -1,9 +1,5 @@
-
-
 class Cli 
-   
       def run
-  
            welcome 
           login_or_create_account 
        #    interest
@@ -26,10 +22,10 @@ class Cli
     def login_or_create_account 
         answer =  $prompt.select("what would you like to do?",%w(LOG_IN CREATE_A_NEW_ACCOUNT))
     if answer == "LOG_IN"
-         @respond =  $prompt.ask('What is your name? >>>') do |user|
-         user.required true
-         user.messages[:required?] = "Enter a username to continue...  "
-         user.messages[:valid?] = "Username taken, please try again..."
+         @respond =  $prompt.ask('What is your name? >>>') do |user1|
+         user1.required true
+         user1.messages[:required?] = "Enter a username to continue...  "
+         user1.messages[:valid?] = "Username taken, please try again..."
       end 
     if @user = User.find_by(name:@respond)
        @user = User.find_by(name:@respond)
@@ -39,9 +35,8 @@ class Cli
               user_book
               user_film 
               user_photography
-              log_in_user_table 
-              do_next
-              create_meetup 
+             # log_in_user_table 
+             updated_table 
      else 
              puts "You don't have an account, create a new account "
              login_or_create_account
@@ -49,63 +44,118 @@ class Cli
     
     
      elsif answer == "CREATE_A_NEW_ACCOUNT"
-          @respond =  $prompt.ask('What is your name? >>>') do |user|
+          @respond_new =  $prompt.ask('What is your name? >>>') do |user|
          user.required true
          user.messages[:required?] = "Enter a username to continue... "
          user.messages[:valid?] = "Username taken, please try again..."
           end
-        if  @user = User.create(name:@respond)
+        if  @person = User.create(name:@respond_new)
           
           
           puts "------------------------------------------------------------------------------------------"
-          puts "                                       WELCOME  #{@user.name}                             "
+          puts "                                       WELCOME  #{@person.name}                             "
           puts "------------------------------------------------------------------------------------------"
-         interest
-         create_meetup 
+         
+         
          
          end 
-         do_next
        end 
+       interest
+       create_meetup
+       do_next
     end 
     # def new_user_meetup
     #   binding.pry
     #   @userb = @person.meetups.find{|meetup| meetup}
     # end
+    def new_user_update_book
+       @respond.meetups do |meetup|
+        meetup.book = @respond_interest1
+        meetup.save
+      end
+    end 
+   #------------------------------------------------------------------------------------user---------------------------------------------------------------------------------------
 
     def user_meetup
-      @usera =  @user.meetups.find{|meetup| meetup}
+      @user_meetup =  @user.meetups
+      # p @user_meetup[0].book
     end
-    def save
-      @usera.user.name = @respond
-    end 
 
-    def user_book
-        
-        puts "#{@respond}'s  book club #{user_meetup.book} "
-    end 
+    def user_books
+      # array of books
+      @user_books = user_meetup.map do |meetup|
+        meetup.book
+      end
+    end
 
-    def user_film 
-      puts "#{@respond}'s  film club #{user_meetup.film} "
+    def user_films
+      user_meetup.map do |meetup|
+        meetup.film
+      end
+    end
+    def user_film
+      user_films.each do |film|
+        puts "#{@respond}'s film clubs: #{film}"
+      end
+    end
+    
+    def user_photographies
+        user_meetup.map do |meetup|
+          meetup.photography
+      end 
     end 
 
     def user_photography
-      puts "#{@respond}'s  photography club #{user_meetup.photography} "
+      user_photographies.map do |photography|
+      puts "#{@respond}'s photography clubs: #{photography}"
+      end 
     end 
+
+
+    def user_book
+       user_books.each do |book|
+      puts "#{@respond}'s book clubs: #{book}"
+    end
+  end 
+
+    # def user_meetup
+    #   @usera =  @user.meetups.find{|meetup| meetup}
+    # end
+    # def save
+    #   @usera.user.name = @respond
+    # end 
+
+    # def user_book
+        
+    #     puts "#{@respond}'s  book club #{user_meetup.book} "
+    # end 
+
+    # def user_film 
+    #   puts "#{@respond}'s  film club #{user_meetup.film} "
+    # end 
+
+    # def user_photography
+    #   puts "#{@respond}'s  photography club #{user_meetup.photography} "
+    # end 
     def interest 
       
          answer = $prompt.select("What is your interest? >>>", %w(book film business tech art photography learning))
         if answer == "book" 
          puts "this week's book of meetup group "
          @respond_interest1 = $prompt.select('please choose one of these book to attend the meet up group', %w(learn_ruby innocent_guy imposible_love pure_love lost_island king_arthur )) 
+         update_book 
         #  add_meetup =  Meetup.find_or_create_by(book: @respond_interest1 ||  user_meetup.book )
+
         #  puts "You are going to attend #{add_meetup[:book]} book review ,this group has #{add_meetup[:capacity]} person from meetup."
         #  puts "if you will attend this group, company gives  #{add_meetup[:discount_percent]} percent off for #{add_meetup[:book]} book "
         #  puts "Also we have this week event is  #{add_meetup[:this_weeks_event]}  that you might be interested  "
+         
       
       elsif answer == "film" 
       puts "this week's film of meetup group"
       @respond_interest2 = $prompt.select('please choose one of these activity to attend the meet up group', %w(three_monkey gora  god_father flight sully unbreakable jumanji toy_story 1917 invisible_man star_wars forrest_gump a_beautiful_mind ))
-      # add_meetup1 = Meetup.find_or_create_by(film: @respond_interest2 || user_meetup.film )
+      update_film 
+       # add_meetup1 = Meetup.find_or_create_by(film: @respond_interest2 || user_meetup.film )
       # puts "You are going to attend #{add_meetup1[:film]} book review ,this group has #{add_meetup1[:capacity]} person from meetup."
       # puts "if you will attend this group, company gives  #{add_meetup1[:discount_percent]} percent off for #{add_meetup1[:book]}  book"
       # puts "Also we have this week event is  #{add_meetup1[:this_weeks_event]}  that you might be interested"
@@ -114,63 +164,98 @@ class Cli
      elsif answer == "photography"
       puts "this week's photograpy of meetup groups"
       @respond_interest3 = $prompt.select('please choose one of these activity to attend the meet up group', %w(brooklyn_museum digital_photography fashion_photography jersey_city_photography hiking_photography landscape_photography))
+      update_photography
       # add_meetup2 = Meetup.find_or_create_by(photography: @respond_interest3 || user_meetup.photography )
       # puts "You are going to attend #{add_meetup2[:photography]} meetup ,this group has #{add_meetup2[:capacity]} person from meetup."
       # puts "if you will attend this group, company gives  #{add_meetup2[:discount_percent]} percent off for #{add_meetup2[:film]}  film"
       # puts "Also we have this week event is  #{add_meetup2[:this_weeks_event]}   that you might be interested"
-     else 
-        update
      end
-       @meetup = Meetup.create(book:@respond_interest1 || user_meetup.book, film:@respond_interest2 || user_meetup.film,photography:@respond_interest3 || user_meetup.photography)
+    #  @meetup = Meetup.find_or_create_by(book:@respond_interest1 || user_book, film:@respond_interest2 || user_film,photography:@respond_interest3 || user_photography)
+      
        do_next 
+      #  update
    end 
     def log_in_user_update
-      update
+      # update
       puts "----------------------------CLICK ON INTEREST THAT YOU WOULD LIKE TO UPDATE--------------------------------------------"
       interest
-      update
+      # update
     
       
     end 
 
-    def update
-      if user_meetup.update(book:@respond_interest1)
-        user_meetup.update(book:@respond_interest1)
-       else 
-        user_meetup.book
-      end 
-
-      if user_meetup.update(film:@respond_interest2)
-        user_meetup.update(film:@respond_interest2)
-      else
-        user_meetup.film
-      end
-
-      if user_meetup.update(photography:@respond_interest3)
-        user_meetup.update(photography:@respond_interest3)
-      else
-        user_meetup.book
-      end 
-
+    def update_book
+     @user.meetups do |meetup|
+      meetup.book = @respond_interest1
+      meetup.save 
+     end
     end 
+
+    def update_film 
+      @user.meetups do |meetup|
+        meetup.film = @respond_interest2
+        meetup.save
+      end 
+    end 
+
+    def update_photography
+      @user.meetups do |meetup|
+        meetup.photography = @respond_interest3
+        meetup.save
+      end 
+    end 
+
+    
+
+
+    
+        #  user = User.find_by(name: 'David')
+        #       user.name = 'Dave'
+        #        user.save 
+
+
+
+
+
+
+    # def update
+    #   if user_book.update(book:@respond_interest1)
+    #     user_book.update(book:@respond_interest1)
+    #    else 
+    #     user_book
+    #   end 
+
+    #   if user_film.update(film:@respond_interest2)
+    #     user_film.update(film:@respond_interest2)
+    #   else
+    #     user_film
+    #   end
+
+    #   if user_photography.update(photography:@respond_interest3)
+    #     user_photography.update(photography:@respond_interest3)
+    #   else
+    #     user_photography
+    #   end 
+
+    # end 
 
 
 
      def do_next
-         update
+        
       $prompt.select("what would you like to do next?", filter: true) do |action|
               
               action.choice 'return main menu', -> {interest}
               action.choice 'exit', -> {exit_meetup}
               action.choice 'delete_account', -> {delete}
-              action.choice 'see your meetups', -> {create_meetup}
-              action.choice 'uptade a meetup', -> {log_in_user_update}
+              action.choice 'see your meetups', -> {updated_table}
+              # action.choice 'uptade a meetup', -> {updated_table}
           end 
 
      end 
 
      def exit_meetup 
-       update
+      
       puts $pastel.red($font.write("                                           GOOD"))
       puts $pastel.red($font.write("                                  BYE    (^_^)"))
       login_or_create_account 
@@ -211,33 +296,46 @@ class Cli
 
 
      def log_in_user_table 
-      update
+      
                         puts "-------------------------------------------------------------------"
                         puts "                        #{@user.name} 'S MEET UP GROUPS            "
                         puts "-------------------------------------------------------------------"
-                        puts "                        #{user_meetup.book}                        "
+                                                       user_book                                
                         puts "-------------------------------------------------------------------" 
-                        puts "                        #{user_meetup.film}                        "
+                                                       user_film                               
                         puts "-------------------------------------------------------------------"
-                        puts "                        #{user_meetup.photography}                 "
+                                                    user_photography 
                         puts "-------------------------------------------------------------------"
      end 
 
+     def updated_table 
 
+                         puts "-------------------------------------------------------------------"
+                         puts "                        #{@user.name} 'S MEET UP GROUPS            "
+                         puts "-------------------------------------------------------------------"
+                                                      @respond_interest1 || user_book                       
+                         puts "-------------------------------------------------------------------" 
+                                                      @respond_interest2  || user_film                           
+                         puts "-------------------------------------------------------------------"
+                                                      @respond_interest3 || user_photography
+                         puts "-------------------------------------------------------------------"
+
+
+     end 
 
    
 
     def create_meetup 
-         @meetup
+        #  @meetup
 
                          puts "-------------------------------------------------------------------"
                          puts "                        #{@respond} 'S MEET UP GROUPS              "
                          puts "-------------------------------------------------------------------"
-                         puts "         #{@respond_interest1 || user_meetup.book}                 "
+                                                               user_book                              
                          puts "-------------------------------------------------------------------" 
-                         puts "         #{@respond_interest2 || user_meetup.film}                 "
+                                                               user_film                              
                          puts "-------------------------------------------------------------------"
-                         puts "         #{@respond_interest3 || user_meetup.photography}          "
+                                                            user_photography                              
                          puts "-------------------------------------------------------------------"
     end 
 
@@ -245,6 +343,24 @@ class Cli
       @user.meetups.select do |meetup|
         meetup == @meetup 
       end
-      create_meetup
-    end 
+
+    end
  end
+
+
+ 
+
+
+
+
+
+ 
+
+    
+ 
+
+
+
+ 
+
+    
--- db/schema.rb
+++ db/schema.rb
@@ -2,43 +2,43 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200324021915) do
+ActiveRecord::Schema.define(version: 2020_03_24_021915) do
 
   create_table "companies", force: :cascade do |t|
-    t.string  "name"
-    t.string  "location"
-    t.string  "category"
+    t.string "name"
+    t.string "location"
+    t.string "category"
     t.integer "capacity"
   end
 
   create_table "meetups", force: :cascade do |t|
     t.integer "capacity"
     t.integer "discount_percent"
-    t.string  "this_weeks_event"
-    t.string  "book"
-    t.string  "film"
-    t.string  "business"
-    t.string  "tech"
-    t.string  "art"
-    t.string  "photography"
-    t.string  "learning"
+    t.string "this_weeks_event"
+    t.string "book"
+    t.string "film"
+    t.string "business"
+    t.string "tech"
+    t.string "art"
+    t.string "photography"
+    t.string "learning"
     t.integer "user_id"
     t.integer "company_id"
-    t.string  "single_club"
+    t.string "single_club"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string  "interest"
+    t.string "interest"
     t.integer "age"
-    t.string  "name"
+    t.string "name"
   end
 
 end
